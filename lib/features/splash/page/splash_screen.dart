ca3ba0ea150f2fd113f590d1e4dd222aa7c03779
johnmikheal <johnmikheal4@gmail.com>
2025-09-26 20:25:51 +0300

    splash_screen

---
+++ lib/features/splash/page/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:medigo/core/constatnts/images.dart';
+import 'package:medigo/core/utils/colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      body: Center(
+        child: Column(
+          children: [
+            SvgPicture.asset(AppImages.appLogoSVG, width: 200),
+            Text(
+              'Doctor Hunt',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
