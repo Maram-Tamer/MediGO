ce2360fb0536862c4aff9ecda226caaa848ba2ec
Noor El Deen <nooraldeinrmadan@gmail.com>
2025-10-13 00:51:39 +0300

    added buttom sheet

---
+++ lib/features/hospital_data/presentation/widgets/hospital_detail_tile.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:medigo/core/utils/colors.dart';
+import 'package:medigo/core/utils/fonts.dart';
+
+class HospitalDetailsTile extends StatelessWidget {
+  const HospitalDetailsTile({
+    super.key,
+    required this.text,
+     required this.icon,
+  });
+  final String text;
+   final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SvgPicture.asset(
+          icon,
+          colorFilter: ColorFilter.mode(
+            AppColors.primaryGreenColor,
+            BlendMode.srcIn,
+          ),
+          height: 25,
+          width: 25,
+        ),
+        Gap(10),
+        Text(
+          text,
+          maxLines: 2,
+          style: AppFontStyles.getSize16(fontColor: AppColors.darkColor),
+        ),
+      ],
+    );
+  }
+}
